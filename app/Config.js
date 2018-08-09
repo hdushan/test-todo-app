71119aeea2edfd53eaac9dc7d0c58a3f8c5d2d66
François Wouts <francois.wouts@airtasker.com>
2018-08-09 11:21:10 +1000

    Update Access-Control-Allow-Origin to use external host

--- app/Config.js
+++ app/Config.js
@@ -1,5 +1,12 @@
+const DB_HOST = process.env.DB_HOST || "localhost:27017";
+const DB_NAME = process.env.DB_NAME || "todos";
+const APP_PORT = parseInt(process.env.APP_PORT) || 4000;
+const FRONTEND_HOST =
+  process.env.FRONTEND_HOST || `http://localhost:${APP_PORT}`;
+
 module.exports = {
-  DB_HOST: process.env.DB_HOST || 'localhost:27017',
-  DB_NAME: process.env.DB_NAME || 'todos',
-  APP_PORT: parseInt(process.env.APP_PORT) || 4000
-}
+  DB_HOST,
+  DB_NAME,
+  APP_PORT,
+  FRONTEND_HOST
+};
--- server.js
+++ server.js
@@ -1,59 +1,59 @@
 /* jslint node: true */
-'use strict'
+"use strict";
 
-var express = require('express')
-
-var morgan = require('morgan')
-
-var path = require('path')
-
-var app = express()
-
-var mongoose = require('mongoose')
-
-var bodyParser = require('body-parser')
+var express = require("express");
+var morgan = require("morgan");
+var path = require("path");
+var app = express();
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 
 // Require configuration file defined in app/Config.js
-var config = require('./app/Config')
+var config = require("./app/Config");
 
 // Connect to database
-mongoose.connect(`mongodb://${config.DB_HOST}/${config.DB_NAME}`)
+mongoose.connect(`mongodb://${config.DB_HOST}/${config.DB_NAME}`);
 
 // Sends static files  from the public path directory
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Use morgan to log request in dev mode
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var port = config.APP_PORT
+// Listen on port defined in config file
+app.listen(config.APP_PORT, () => {
+  console.log("App listening on port " + config.APP_PORT);
+});
 
-app.listen(port) // Listen on port defined in config file
-
-console.log('App listening on port ' + port)
-
-var todoRoutes = require('./app/Routes')
+var todoRoutes = require("./app/Routes");
 
 //  Use routes defined in Route.js and prefix with todo
-app.use('/api', todoRoutes)
+app.use("/api", todoRoutes);
 
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + port)
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", config.FRONTEND_HOST);
 
-    // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
-    // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
 
-    // Pass to next layer of middleware
-  next()
-})
+  // Pass to next layer of middleware
+  next();
+});
 // Server index.html page when request to the root is made
-app.get('/', function (req, res, next) {
-  res.sendfile('./public/index.html')
-})
+app.get("/", function(req, res, next) {
+  res.sendfile("./public/index.html");
+});
